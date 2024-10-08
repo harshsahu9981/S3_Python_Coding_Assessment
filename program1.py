8802d045c42a2b5013c949659ed5700a89c09c4f
harshsahu9981 <150239841+harshsahu9981@users.noreply.github.com>
2024-10-08 15:46:17 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,20 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def isValid(s: str) -> bool:
+    # Dictionary to hold the matching pairs
+    matching_brackets = {')': '(', '}': '{', ']': '['}
+    stack = []
 
-
-
-
-
-
-
+    for char in s:
+        if char in matching_brackets.values():
+            # If it's an opening bracket, push to stack
+            stack.append(char)
+        elif char in matching_brackets.keys():
+            # If it's a closing bracket, check the stack
+            if stack == [] or stack.pop() != matching_brackets[char]:
+                return False
+        else:
+            # Invalid character (not expected in this problem)
+            return False
     
-
-
-
-  
-
+    # If the stack is empty, all brackets matched correctly
+    return stack == []
